bf3ad85cd0a4a55352135ef57ff33f24f96ea8bd
jingmeimei <jingmeimei@kuaishou.com>
2020-08-03 15:42:54 +0800

    深克隆

--- 前端常见手写代码/deepClone.js
+++ 前端常见手写代码/deepClone.js
@@ -13,9 +13,17 @@ function deepClone(target) {
 const oldObj = {
     a: 1,
     b: ['e', 'f', 'g'],
-    c: {h: {i: 2}}
+    c: {h: {i: 2}},
+    sum : function () {
+        console.log(this.a);
+    }
 };
 const newObj = deepClone(oldObj);
 newObj.c.h.i = 3;
 console.log(oldObj.c);
 console.log(newObj.c);
+// 方法二
+const news = JSON.parse(JSON.stringify(oldObj));
+newObj.c.h.i = 3;
+console.log(oldObj.sum());
+console.log(newObj.sum());
